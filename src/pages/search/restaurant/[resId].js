6aeaa689a4455c81608e148dd8dae48296de46fb
folkmoz1 <folkmoz1@gmail.com>
2020-09-18 01:18:34 +0700

    +

--- src/pages/search/restaurant/[resId].js
+++ src/pages/search/restaurant/[resId].js
@@ -44,43 +44,6 @@ const Restaurant = () => {
 }
 
 
-/*
-export const getStaticPaths = async () => {
-
-    return {
-        paths:[
-            {params:{resId: "P08000799"}}
-        ],
-        fallback: true
-    }
-}
-
-export const getStaticProps = async ({ params: { resId } }) => {
-    let result;
-
-    try {
-        const res = await get(`https://tatapi.tourismthailand.org/tatapi/v5/restaurant/${resId}`,{
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                'Authorization': `Bearer ${process.env.NEXT_PUBLIC_MY_API_KEY}`,
-                'Accept-Language': 'th'
-            }
-        })
-
-        if (!res) throw new Error('Cannot fetch data.')
-
-        result = res.data.result
-    } catch (err) {
-        console.log(err)
-    }
-
-    return {
-        props: {
-            result
-        }
-    }
-}
-*/
 
 
 export default Restaurant;
